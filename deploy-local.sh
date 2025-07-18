4b3719190a21fec993d1d13b7646129c4a1539b1
bakralakhras <bakrferas@gmail.com>
2025-07-19 01:47:21 +0300

    fixed script

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+
 CHART_PATH=./helm/incidentvault
 RELEASE_NAME=incidentvault
+DOCKER_USER=bakrferas
 IMAGE_NAME=incidentvault
 SHA=$(git rev-parse --short HEAD)
 
-echo "Building Docker image: ${IMAGE_NAME}:${SHA}"
-docker build -f docker/Dockerfile -t ${IMAGE_NAME}:${SHA} .
+echo "Pulling image ${DOCKER_USER}/${IMAGE_NAME}:${SHA} from Docker Hub..."
+docker pull ${DOCKER_USER}/${IMAGE_NAME}:${SHA}
 
-echo "Loading image into Minikube..."
-minikube image load ${IMAGE_NAME}:${SHA}
+echo "Loading image into Minikube cache..."
+minikube image load ${DOCKER_USER}/${IMAGE_NAME}:${SHA}
 
-echo "Deploying with Helm..."
+echo "Deploying via Helm..."
 helm upgrade --install ${RELEASE_NAME} ${CHART_PATH} \
-  --set image.repository=${IMAGE_NAME} \
-  --set image.tag=${SHA}
+  --set image.repository=${DOCKER_USER}/${IMAGE_NAME} \
+  --set image.tag=${SHA} \
+  --set image.pullPolicy=IfNotPresent
 
-echo "Forwarding port 8000 to service/${RELEASE_NAME}"
+echo "Port forwarding 8000 -> service/${RELEASE_NAME}"
 kubectl port-forward svc/${RELEASE_NAME} 8000:8000
