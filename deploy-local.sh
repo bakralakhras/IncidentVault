dc873240b14d8e925d88db1f0b465a9556d68c1a
bakralakhras <bakrferas@gmail.com>
2025-07-19 01:34:35 +0300

    fixed script

--- deploy-local.sh
+++ deploy-local.sh
@@ -3,7 +3,17 @@ CHART_PATH=./helm/incidentvault
 RELEASE_NAME=incidentvault
 IMAGE_NAME=incidentvault
 SHA=$(git rev-parse --short HEAD)
-echo "👉 Using image tag: $SHA"
+
+echo "Building Docker image: ${IMAGE_NAME}:${SHA}"
+docker build -f docker/Dockerfile -t ${IMAGE_NAME}:${SHA} .
+
+echo "Loading image into Minikube..."
 minikube image load ${IMAGE_NAME}:${SHA}
-helm upgrade --install ${RELEASE_NAME} ${CHART_PATH} --set image.repository=${IMAGE_NAME} --set image.tag=${SHA}
+
+echo "Deploying with Helm..."
+helm upgrade --install ${RELEASE_NAME} ${CHART_PATH} \
+  --set image.repository=${IMAGE_NAME} \
+  --set image.tag=${SHA}
+
+echo "Forwarding port 8000 to service/${RELEASE_NAME}"
 kubectl port-forward svc/${RELEASE_NAME} 8000:8000
