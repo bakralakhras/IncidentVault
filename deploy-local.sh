1f4de7b16b9b0f4db713d39bab353eb724c54931
bakralakhras <bakrferas@gmail.com>
2025-07-19 01:52:30 +0300

    script 8000 kill

--- deploy-local.sh
+++ deploy-local.sh
@@ -5,23 +5,32 @@ RELEASE_NAME=incidentvault
 DOCKER_USER=bakrferas
 IMAGE_NAME=incidentvault
 
-echo "📥 Pulling latest code from GitHub..."
+echo "Pulling latest code from GitHub..."
 git pull origin main
 
 SHA=$(git rev-parse --short HEAD)
-echo "🔖 Using image tag: ${SHA}"
+echo "Using image tag: ${SHA}"
 
-echo "📥 Pulling Docker image ${DOCKER_USER}/${IMAGE_NAME}:${SHA} from Docker Hub..."
+echo "Pulling Docker image ${DOCKER_USER}/${IMAGE_NAME}:${SHA} from Docker Hub..."
 docker pull ${DOCKER_USER}/${IMAGE_NAME}:${SHA}
 
-echo "📦 Loading image into Minikube..."
+echo "Loading image into Minikube..."
 minikube image load ${DOCKER_USER}/${IMAGE_NAME}:${SHA}
 
-echo "🚀 Deploying via Helm..."
+echo "Deploying via Helm..."
 helm upgrade --install ${RELEASE_NAME} ${CHART_PATH} \
   --set image.repository=${DOCKER_USER}/${IMAGE_NAME} \
   --set image.tag=${SHA} \
   --set image.pullPolicy=IfNotPresent
+
+echo "Killing any process using port 8000..."
+PID=$(lsof -ti:8000)
+if [ -n "$PID" ]; then
+  kill -9 $PID
+  echo "Killed process $PID"
+else
+  echo " No process found on port 8000"
+fi
 
 echo "🔌 Port forwarding 8000 -> service/${RELEASE_NAME}"
 kubectl port-forward svc/${RELEASE_NAME} 8000:8000
